2d87b4d75793927d65036dfcc09047aed24b0041
nfl_shadow <nfl_shadow@hotmail.com>
2018-09-09 23:24:14 +0200

    add snowboy

---
+++ app/src/com/adventiel/selena/MyPreferencesActivity.java
@@ -0,0 +1,35 @@
+package com.adventiel.selena;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
